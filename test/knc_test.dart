47a129c3119d71e53fdc065a7afb144ddce86aab
Atom735 <Atom735.ag47@gmail.com>
2020-06-30 14:58:59 +0300

    try to use split on strings

--- test/knc_test.dart
+++ test/knc_test.dart
@@ -5,4 +5,17 @@ void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  test('string split', () {
+    final str =
+        '  1111.111  2222    444 333.412 666   809123.213 123       1923 81 94.3';
+    print(str.split(' '));
+    var i = 0;
+    str.split(' ').forEach((e) {
+      if (e.isNotEmpty) {
+        i += 1;
+        print('$i: "$e"');
+      }
+    });
+  });
 }
