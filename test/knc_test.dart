187eb737fb24872b1989982eebe4ed2e77c9628a
Atom735 <Atom735.ag47@gmail.com>
2020-06-30 17:54:00 +0300

    test future how it work

--- test/knc_test.dart
+++ test/knc_test.dart
@@ -1,3 +1,5 @@
+import 'dart:cli';
+
 import 'package:knc/knc.dart';
 import 'package:test/test.dart';
 
@@ -17,5 +19,37 @@ void main() {
         print('$i: "$e"');
       }
     });
+  });
+
+  test('futures', () {
+    final f1 = Future.value(42);
+    final f2 = f1.then((e) {
+      print('f1 begin');
+      return Future.delayed(Duration(milliseconds: 300), () {
+        print('f1 begin inner 1');
+        return Future.delayed(Duration(milliseconds: 333), () {
+          print('f1 begin inner 2');
+          return 'end';
+        });
+      });
+    });
+    final f3 = Future.delayed(Duration(milliseconds: 166), () {
+      print('f3 complete');
+      final f4 = Future.delayed(Duration(milliseconds: 666), () {
+        print('f4');
+        return Future.delayed(Duration(milliseconds: 33), () {
+          print('f4 inner');
+          return 'e2';
+        });
+      });
+      print('f4 created');
+      return f4;
+    });
+    final f5 = Future.wait([f1, f2, f3]).then((value) {
+      print(value);
+      print('its all');
+    });
+    print('test end');
+    print(waitFor(f5));
   });
 }
