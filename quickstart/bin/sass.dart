008ef76f07bcdc4e5503f47b74753cf25d8e52f4
Atom735 <Atom735.ag47@gmail.com>
2020-07-29 20:16:41 +0300

    add newTaskDialog

--- quickstart/bin/sass.dart
+++ quickstart/bin/sass.dart
@@ -3,49 +3,51 @@ import 'package:path/path.dart' as p;
 import 'package:sass/sass.dart' as sass;
 import 'package:package_resolver/package_resolver.dart';
 
+const map = <String, List<String>>{
+  'main.scss': ['_app.scss']
+};
+
 Future main(List<String> args) async {
   print('sass begin watch!');
-  final dir = Directory(p.join(Directory.current.path, 'web'));
   final packageResolver =
       await SyncPackageResolver.loadConfig(Uri.file('.packages'));
-  //C:\Users\User4\AppData\Local\Pub\Cache\hosted\pub.dartlang.org
+  final dir = Directory(p.join(Directory.current.path, 'web'));
+
+  Future sassCompile(String path) async {
+    final relative = path.substring(dir.path.length + 1);
+    try {
+      final css = File(p.setExtension(path, '.css'));
+      await css.writeAsString(
+          await sass.compileAsync(path, packageResolver: packageResolver));
+      print('sass: compiled ${path} => ${css.path}');
+      final list = <String>[];
+      map.forEach((key, value) {
+        if (value.contains(relative)) {
+          list.add(key);
+        }
+      });
+      for (final l in list) {
+        await sassCompile(p.join(dir.path, l));
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   await dir.list(recursive: true).listen((entity) async {
     if (entity is File && p.extension(entity.path) == '.scss') {
-      try {
-        final css = File(p.setExtension(entity.path, '.css'));
-        await css.writeAsString(await sass.compileAsync(entity.path,
-            packageResolver: packageResolver));
-        print('sass: compiled ${entity.path} => ${css.path}');
-      } catch (e) {
-        print(e);
-      }
+      await sassCompile(entity.path);
     }
   }).asFuture();
-  dir.watch(recursive: true).listen((event) async {
+  await dir.watch(recursive: true).listen((event) async {
     if (p.extension(event.path) == '.scss') {
-      final css = File(p.setExtension(event.path, '.css'));
       switch (event.type) {
         case FileSystemEvent.create:
         case FileSystemEvent.modify:
-          try {
-            await css.writeAsString(await sass.compileAsync(event.path,
-                packageResolver: packageResolver));
-            print('sass: compiled ${event.path} => ${css.path}');
-          } catch (e) {
-            print(e);
-          }
-          break;
-        case FileSystemEvent.delete:
-          try {
-            await css.delete();
-            print('sass: deleted ${css.path}');
-          } catch (e) {
-            print(e);
-          }
+          await sassCompile(event.path);
           break;
         default:
       }
     }
-  });
+  }).asFuture();
 }
--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -1,7 +1,100 @@
+import 'dart:html';
+
 import 'package:m4d_core/m4d_ioc.dart' as ioc;
 import 'package:m4d_components/m4d_components.dart';
+
+/// webdev serve --auto refresh --debug --launch-in-chrome --log-requests
+
+class TaskPath {
+  int i;
+  String value = '';
+  TableRowElement eTr;
+  InputElement eInput;
+  Element eButton;
+}
 
 Future main() async {
   ioc.Container.bindModules([CoreComponentsModule()]);
   await componentHandler().upgrade();
+
+  final DialogElement dialogTaskDetails =
+      document.getElementById('task-details');
+  final ButtonElement btnCloseTaskDetails =
+      document.getElementById('task-details-btn-close');
+  btnCloseTaskDetails.onClick.listen((_) {
+    dialogTaskDetails.close();
+  });
+
+  final DialogElement dialogNewTaskSets =
+      document.getElementById('new-task-sets');
+  final ButtonElement btnCloseNewTaskSets =
+      document.getElementById('new-task-sets-btn-close');
+  btnCloseNewTaskSets.onClick.listen((_) {
+    dialogNewTaskSets.close();
+  });
+
+  final newTaskPathList = <TaskPath>[];
+
+  final InputElement newTaskName = document.getElementById('new-task-name');
+
+  final TableElement tableNewTaskPaths =
+      document.getElementById('new-task-path-table');
+  void tableNewTaskPathsAddNew() {
+    var i = newTaskPathList.indexOf(null);
+    final taskPath = TaskPath();
+    if (i == -1) {
+      i = newTaskPathList.length;
+      newTaskPathList.add(taskPath);
+    } else {
+      newTaskPathList[i] = taskPath;
+    }
+
+    taskPath.i = i;
+    tableNewTaskPaths.appendHtml('''
+            <tr id="new-task-path${i}-div">
+              <td>
+                <div class="mdl-textfield">
+                  <input class="mdl-textfield__input" type="text"
+                    id="new-task-path${i}">
+                  <label class="mdl-textfield__label" for="new-task-path${i}">
+                    Путь к папке или файлу для обработки...</label>
+                </div>
+              </td>
+              <td><button class="mdl-button mdl-button--icon"
+                  id="new-task-path${i}-remove">
+                  <i class="material-icons">remove</i>
+                </button></td>
+            </tr>
+  ''');
+    taskPath.eTr = document.getElementById('new-task-path${i}-div');
+    taskPath.eInput = document.getElementById('new-task-path${i}');
+    taskPath.eButton = document.getElementById('new-task-path${i}-remove');
+    taskPath.eButton.onClick.listen((_) {
+      taskPath.eTr.remove();
+      newTaskPathList[i] = null;
+    });
+
+    componentHandler().upgradeElement(tableNewTaskPaths);
+
+    taskPath.eInput.focus();
+  }
+
+  tableNewTaskPathsAddNew();
+
+  final btnNewTaskPathAdd = document.getElementById('new-task-path-add');
+  btnNewTaskPathAdd.onClick.listen((_) => tableNewTaskPathsAddNew());
+
+  final AnchorElement btnAddNewTask = document.getElementById('add-task-btn');
+  btnAddNewTask.onClick.listen((_) {
+    dialogNewTaskSets.showModal();
+    newTaskName.focus();
+  });
+
+  final List<ButtonElement> btnsTaskDetails =
+      document.querySelectorAll<ButtonElement>('.task-btn-details');
+  for (var btn in btnsTaskDetails) {
+    btn.onClick.listen((_) {
+      dialogTaskDetails.showModal();
+    });
+  }
 }
