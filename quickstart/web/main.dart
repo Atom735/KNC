3ecaf8b341c777aa7cbcc14470088e707929157f
Atom735 <Atom735.ag47@gmail.com>
2020-07-29 21:42:47 +0300

    try to go to OOP

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -5,12 +5,91 @@ import 'package:m4d_components/m4d_components.dart';
 
 /// webdev serve --auto refresh --debug --launch-in-chrome --log-requests
 
-class TaskPath {
-  int i;
+class TaskState {
+  int id;
+  String name;
+  int state;
+  // TODO: details
+}
+
+Element eGetById(final String id) => document.getElementById(id);
+
+class TaskSetsPath {
   String value = '';
-  TableRowElement eTr;
-  InputElement eInput;
-  Element eButton;
+  final int id;
+  final TableRowElement eRow;
+  final InputElement eInput;
+  final Element eRemove;
+  TaskSetsPath(this.id, final List<TaskSetsPath> list)
+      : eRow = eGetById('task-sets-path-${id}-row'),
+        eInput = eGetById('task-sets-path-${id}-input'),
+        eRemove = eGetById('task-sets-path-${id}-remove') {
+    eRemove.onClick.listen((_) {
+      eRow.remove();
+      list[id] = null;
+    });
+    componentHandler().upgradeElement(eRow);
+    eInput.focus();
+  }
+
+  static String html(final int id) => '''
+    <tr id="task-sets-path-${id}-row">
+      <td>
+        <div class="mdl-textfield">
+          <input id="task-sets-path-${id}-input" class=" mdl-textfield__input"
+            type="text">
+          <label for="task-sets-path-${id}-input" class="mdl-textfield__label">
+            Путь к папке или файлу для обработки...
+          </label>
+        </div>
+      </td>
+      <td><button id="task-sets-path-${id}-remove"
+          class="mdl-button mdl-button--icon">
+          <i class="material-icons">remove</i>
+        </button></td>
+    </tr>
+  ''';
+}
+
+class TaskSetsDialog {
+  final DialogElement eDialog;
+  final ButtonElement eClose;
+  final ButtonElement eStart;
+  final InputElement eName;
+  final TableElement eTable;
+  final ButtonElement eOpen;
+  final ButtonElement ePath;
+
+  final pathList = <TaskSetsPath>[];
+
+  Future start() async {
+    // TODO: отправка данных
+  }
+
+  void pathAdd() {
+    var i = pathList.indexOf(null);
+    if (i == -1) {
+      i = pathList.length;
+      pathList.add(null);
+    }
+    eTable.appendHtml(TaskSetsPath.html(i));
+    pathList[i] = TaskSetsPath(i, pathList);
+  }
+
+  TaskSetsDialog()
+      : eDialog = eGetById('task-sets-dialog'),
+        eClose = eGetById('task-sets-close'),
+        eStart = eGetById('task-sets-start'),
+        eName = eGetById('task-sets-name'),
+        eTable = eGetById('task-sets-table'),
+        eOpen = eGetById('task-sets-open'),
+        ePath = eGetById('task-sets-path') {
+    eClose.onClick.listen((_) => eDialog.close());
+    eStart.onClick.listen((_) => start());
+    eOpen.onClick.listen((_) => eDialog.showModal());
+    ePath.onClick.listen((_) => pathAdd());
+    pathAdd();
+  }
 }
 
 Future main() async {
@@ -32,8 +111,13 @@ Future main() async {
   btnCloseNewTaskSets.onClick.listen((_) {
     dialogNewTaskSets.close();
   });
+  final ButtonElement btnStartNewTask =
+      document.getElementById('new-task-sets-btn-start');
+  btnStartNewTask.onClick.listen((_) {
+    dialogNewTaskSets.close();
+  });
 
-  final newTaskPathList = <TaskPath>[];
+  final newTaskPathList = <TaskSetsPath>[];
 
   final InputElement newTaskName = document.getElementById('new-task-name');
 
@@ -41,7 +125,7 @@ Future main() async {
       document.getElementById('new-task-path-table');
   void tableNewTaskPathsAddNew() {
     var i = newTaskPathList.indexOf(null);
-    final taskPath = TaskPath();
+    final taskPath = TaskSetsPath();
     if (i == -1) {
       i = newTaskPathList.length;
       newTaskPathList.add(taskPath);
