9a573a9bc0ba77fde1be1dab187a02b12417e6f3
Atom735 <Atom735.ag47@gmail.com>
2020-08-03 19:34:34 +0300

    fix and add future to socket wrapper

--- quickstart/web/socketWrapper.dart
+++ quickstart/web/socketWrapper.dart
@@ -1,33 +1,50 @@
 import 'dart:async';
 
+class SocketWrapperResponse {
+  final String s;
+  final int i;
+  SocketWrapperResponse(this.s, this.i);
+}
+
 /// Оболочка для взаимодействия передочи потоковых данных
 ///
-/// * [send] - функция отправки сообщения - должна быть задана
+/// * [sender] - функция отправки сообщения - должна быть задана
 /// * [recv] - функция вызываемаемая при получении сообщения
 class SocketWrapper {
-  final void Function(String msgRaw) send;
+  final void Function(String msgRaw) sender;
   final String streamCloseMsg;
   final String msgIdBegin;
   final String msgIdEnd;
+  Future signal;
 
   int _requestID = 0;
   final _listOfRequest = <int, Completer<String>>{};
   int _subscribersID = 0;
   final _listOfSubscribers = <int, StreamController<String>>{};
 
-  final _listOfResponses = <String, Completer<String>>{};
-  final _listOfRespSubers = <String, StreamController<String>>{};
+  final _listOfResponses = <String, Completer<SocketWrapperResponse>>{};
+  final _listOfRespSubers = <String, StreamController<SocketWrapperResponse>>{};
 
-  SocketWrapper(this.send,
+  SocketWrapper(this.sender,
       {this.streamCloseMsg = 'streamclose',
       this.msgIdBegin = '\u{1}',
-      this.msgIdEnd = '\u{2}'});
+      this.msgIdEnd = '\u{2}',
+      this.signal}) {
+    if (signal != null) {
+      signal.then((_) => signal = null);
+    }
+  }
 
-  void recv(final String msgRaw) {
-    if (msgRaw[0] == msgIdBegin) {
-      final i0 = msgRaw.indexOf(msgIdEnd);
+  /// Функция отправки сообщений с уникальным айди
+  void send(final int id, final String msg) => signal == null
+      ? sender('$msgIdBegin$id$msgIdEnd$msg')
+      : signal.then((_) => sender('$msgIdBegin$id$msgIdEnd$msg'));
+
+  void recv(final String msgRaw, [final int id]) {
+    if (msgRaw.startsWith(msgIdBegin)) {
+      final i0 = msgRaw.indexOf(msgIdEnd, msgIdBegin.length);
       if (i0 != -1) {
-        final id = int.tryParse(msgRaw.substring(0, i0));
+        final id = int.tryParse(msgRaw.substring(msgIdBegin.length, i0));
         final msg = msgRaw.substring(i0 + msgIdEnd.length);
         if (_listOfRequest[id] != null) {
           _listOfRequest[id].complete(msg);
@@ -35,32 +52,32 @@ class SocketWrapper {
         if (_listOfSubscribers[id] != null) {
           _listOfSubscribers[id].add(msg);
         }
-        return recv(msg);
+        return recv(msg, id);
       }
     }
     _listOfResponses.forEach((key, value) {
       if (msgRaw.startsWith(key)) {
-        value.complete(msgRaw.substring(key.length));
+        value.complete(SocketWrapperResponse(msgRaw.substring(key.length), id));
       }
     });
     _listOfRespSubers.forEach((key, value) {
       if (msgRaw.startsWith(key)) {
-        value.add(msgRaw.substring(key.length));
+        value.add(SocketWrapperResponse(msgRaw.substring(key.length), id));
       }
     });
   }
 
   /// Подписаться на получение единождого ответа
-  Future<String> waitMsg(final String msgBegin) {
-    final c = Completer<String>();
+  Future<SocketWrapperResponse> waitMsg(final String msgBegin) {
+    final c = Completer<SocketWrapperResponse>();
     _listOfResponses[msgBegin] = c;
     c.future.then((_) => _listOfResponses.remove(msgBegin));
     return c.future;
   }
 
   /// Подписаться на получение всех ответов
-  Stream<String> waitMsgAll(final String msgBegin) {
-    final c = StreamController<String>(
+  Stream<SocketWrapperResponse> waitMsgAll(final String msgBegin) {
+    final c = StreamController<SocketWrapperResponse>(
         onCancel: () => _listOfRespSubers.remove(msgBegin));
     _listOfRespSubers[msgBegin] = c;
     return c.stream;
@@ -73,7 +90,7 @@ class SocketWrapper {
     final c = Completer<String>();
     _listOfRequest[id] = c;
     c.future.then((_) => _listOfRequest.remove(id));
-    send('$msgIdBegin$id$msgIdEnd$msg');
+    send(id, msg);
     return c.future;
   }
 
@@ -83,10 +100,10 @@ class SocketWrapper {
     final id = _subscribersID;
     final c = StreamController<String>(onCancel: () {
       _listOfSubscribers.remove(id);
-      send('$msgIdBegin$id$msgIdEnd$streamCloseMsg');
+      send(id, streamCloseMsg);
     });
     _listOfSubscribers[id] = c;
-    send('$msgIdBegin$id$msgIdEnd$msg');
+    send(id, msg);
     return c.stream;
   }
 }
