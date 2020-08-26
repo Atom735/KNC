4943b88b64db3925ef7f65c9976a46f65f34dbfb
Atom735 <Atom735.ag47@gmail.com>
2020-08-26 18:56:00 +0300

    try for use history API

---
+++ web/web/test.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+main(List<String> args) {
+  print('Hello World!');
+
+  window.onPopState.listen((event) {
+    print(event.state);
+    print(window.location.href);
+  });
+  window.onLoad.listen((event) {
+    window.history.pushState('Data of state', 'title', '/url');
+  });
+}
