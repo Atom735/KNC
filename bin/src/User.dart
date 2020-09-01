080e718e7936cbb73f13c9c5a4f15c48f7088099
Atom735 <Atom735.ag47@gmail.com>
2020-09-01 15:22:12 +0300

    fix load user file, when it not found

--- bin/src/User.dart
+++ bin/src/User.dart
@@ -32,16 +32,17 @@ class User {
     if (_list.any((e) => e.mail == mail)) {
       return null;
     }
-    final _user = User._(mail, pass);
+    final _user = User._(mail, pass, 'a');
     _list.add(_user);
     save();
     return _user;
   }
 
   /// Загружает данные всех пользователей
-  static Future<void> load() =>
-      File('data/users.json').readAsString().then((data) => _list
-          .addAll((jsonDecode(data) as List).map((e) => User._fromJson(e))));
+  static Future<void> load() => File('data/users.json').exists().then((b) => b
+      ? File('data/users.json').readAsString().then((data) => _list
+          .addAll((jsonDecode(data) as List).map((e) => User._fromJson(e))))
+      : null);
 
   /// Сохраняет данные всех пользователей
   static Future<void> save() =>
