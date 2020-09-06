e7c13194e44da61812e4db6c25686bc9bc3f075d
Atom735 <Atom735.ag47@gmail.com>
2020-09-06 16:03:54 +0300

    use universile mime lib and add cache system

--- bin/src/Server.dart
+++ bin/src/Server.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:crclib/reveng.dart';
 import 'package:knc/knc.dart';
 import 'package:path/path.dart' as p;
+import 'package:mime/mime.dart';
 
 import 'Client.dart';
 
@@ -14,56 +16,80 @@ class Server {
     Directory('tasks').absolute,
     Directory('web').absolute,
   ];
-  final fileMap = <String, File>{'/': File('web/index.html')};
+  final fileMap = <String, File>{'/': File('web/index.html').absolute};
   final reMap = <RegExp, File>{
-    RegExp(r'^\/app(\/.+)?', caseSensitive: false): File('web/index.html')
+    RegExp(r'^\/app(\/.+)?', caseSensitive: false):
+        File('web/index.html').absolute
   };
 
+  final fileMapCache = <File, List<int>>{};
+  final fileMapCrc = <File, int>{};
+
   void serveFile(HttpRequest request, HttpResponse response, File file) {
-    if (file != null && file.existsSync()) {
-      response.statusCode = HttpStatus.ok;
-      final ext = p.extension(file.path).toLowerCase();
-      switch (ext) {
-        case '.html':
-          response.headers.add('content-type', 'text/html');
-          break;
-        case '.css':
-          response.headers.add('content-type', 'text/css');
-          break;
-        case '.js':
-          response.headers.add('content-type', 'application/javascript');
-          break;
-        case '.ico':
-          response.headers.add('content-type', 'image/x-icon');
-          break;
-        case '.map':
-          response.headers.add('content-type', 'application/json');
-          break;
-        case '.xlsx':
-          response.headers.add('content-type', 'application/vnd.ms-excel');
-          break;
-        default:
+    if (file != null) {
+      if (fileMapCrc[file] != null &&
+          fileMapCrc[file] ==
+              int.tryParse(request.headers.value(HttpHeaders.ifNoneMatchHeader),
+                  radix: 16)) {
+        /// Если E-tag совпадает
+        response
+          ..statusCode = HttpStatus.notModified
+          ..flush().then((_) {
+            response.close().then((_) {
+              iReq -= 1;
+              print(
+                  'http(${request.hashCode}): ${request.uri.path} closed ($iReq) not modified');
+            });
+          });
+      } else if (fileMapCache[file] != null) {
+        /// Файл закеширован
+        response
+          ..statusCode = HttpStatus.ok
+          ..headers
+              .add(HttpHeaders.contentTypeHeader, lookupMimeType(file.path))
+          ..headers
+              .add(HttpHeaders.etagHeader, fileMapCrc[file].toRadixString(16))
+          ..add(fileMapCache[file])
+          ..flush().then((_) {
+            response.close().then((_) {
+              iReq -= 1;
+              print(
+                  'http(${request.hashCode}): ${request.uri.path} closed ($iReq) from cache #${fileMapCrc[file].toRadixString(16)}');
+            });
+          });
+      } else if (file.existsSync()) {
+        /// Файл существует
+        file.readAsBytes().then((bytes) {
+          fileMapCache[file] = bytes;
+          fileMapCrc[file] = Crc32().convert(bytes);
+          print('$file cached #${fileMapCrc[file].toRadixString(16)}');
+          serveFile(request, response, file);
+        });
+      } else {
+        /// Файл не наден
+        response
+          ..statusCode = HttpStatus.notFound
+          ..write('404: Not Found')
+          ..flush().then((_) {
+            response.close().then((_) {
+              iReq -= 1;
+              print(
+                  'http(${request.hashCode}): ${request.uri.path} closed ($iReq)');
+            });
+          });
       }
-      response.addStream(file.openRead()).then((_) {
-        response.flush().then((_) {
+    } else {
+      /// Файл не задан
+      response
+        ..statusCode = HttpStatus.notFound
+        ..write('404: Not Found')
+        ..flush().then((_) {
           response.close().then((_) {
             iReq -= 1;
             print(
                 'http(${request.hashCode}): ${request.uri.path} closed ($iReq)');
           });
         });
-      });
-      print('http: ${request.uri.path} serve $file');
-    } else {
-      response.statusCode = HttpStatus.notFound;
-      response.write('404: Not Found');
-      response.flush().then((_) {
-        response.close().then((_) {
-          iReq -= 1;
-          print(
-              'http(${request.hashCode}): ${request.uri.path} closed ($iReq)');
-        });
-      });
     }
   }
 
@@ -80,11 +106,25 @@ class Server {
     print('Listening on http://${server.address.address}:${server.port}/');
     print('For connect use http://localhost:${server.port}/');
     _instance = this;
+
+    for (final dir in dirs) {
+      /// Следим за изменением файлов в папках
+      dir.watch(recursive: true).listen((event) {
+        final file = File(event.path).absolute;
+        if (fileMapCache[file] != null) {
+          fileMapCache.remove(file);
+          fileMapCrc.remove(file);
+          print('$file cached removed');
+        }
+      });
+    }
+
     server.listen((request) {
       iReq += 1;
       final response = request.response;
       print('http(${request.hashCode}): ${request.uri.path} opend ($iReq)');
       if (request.uri.path == '/ws') {
+        /// если подключается сокет
         WebSocketTransformer.upgrade(request).then((websocket) {
           print('http: ${request.uri.path} upgraded to WebSocket');
           Client(websocket);
@@ -97,15 +137,20 @@ class Server {
           });
         });
       } else if (fileMap[request.uri.path] != null) {
+        /// если есть ремап кнкретной ссылки на файл
         serveFile(request, response, fileMap[request.uri.path]);
+        return;
       } else {
-        for (var key in reMap.keys) {
+        /// если есть ремап шаблона ссылки на файл
+        for (final key in reMap.keys) {
           if (key.hasMatch(request.uri.path)) {
             serveFile(request, response, reMap[key]);
             return;
           }
         }
-        for (var dir in dirs) {
+
+        /// если есть необходимый файл в папках
+        for (final dir in dirs) {
           final f = File(dir.path + request.uri.path);
           if (f.existsSync()) {
             serveFile(request, response, f);
