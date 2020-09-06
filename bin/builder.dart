e8753cddf308c8d67219d4fcb47f65aa7eb4326d
Atom735 <Atom735.ag47@gmail.com>
2020-09-06 09:00:52 +0300

    set webdev to rebuild auto

--- bin/builder.dart
+++ bin/builder.dart
@@ -1,15 +1,54 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+Completer completer;
+
+void rebuild() {
+  completer = Completer();
+  print('+completer created');
+  completer.future.then(func);
+  Process.start(
+          'webdev.bat', ['build', '--no-release', '--output', 'web:build'],
+          workingDirectory: Directory.current.path)
+      .then((_) {
+    final s = StringBuffer();
+    _.stdout.listen((e) {
+      final _e = utf8.decode(e);
+      print(_e);
+      s.write(_e);
+    });
+    _.exitCode.then((e) {
+      if (s.toString().contains('Exception')) {
+        recompile = true;
+      }
+      completer?.complete(e);
+    });
+  });
+}
+
+var recompile = false;
+
+void func(_) {
+  completer = null;
+  print('+completer closed');
+  if (recompile) {
+    recompile = false;
+    rebuild();
+  }
+}
+
 Future<int> main(List<String> args) async {
-  final dirBuild = Directory('build');
-  if (!(await dirBuild.exists())) {
-    await dirBuild.create();
-  }
-  final dirWeb = Directory('build/web');
-  if (!(await dirWeb.exists())) {
-    await dirWeb.create();
-  }
-  await Process.run(
-      'dartdevc.bat', ['-k', '-o', 'build/web/main.dart.js', 'web/main.dart']);
+  final dirWeb = Directory('web');
+  dirWeb.watch(recursive: true).listen((event) {
+    print('+event');
+    if (completer == null) {
+      rebuild();
+    } else {
+      recompile = true;
+    }
+  });
+  print('+start');
+  await rebuild();
   return 0;
 }
