b3913c4debd8bfb20d87b41062ef2777c8480997
Atom735 <Atom735.ag47@gmail.com>
2020-09-09 18:49:03 +0300

    add las file viewer

--- web/src/FileLas.dart
+++ web/src/FileLas.dart
@@ -22,24 +22,126 @@ class FileLas {
       if (_msg.isEmpty) {
         return false;
       }
+
       oneFileData = OneFileData.byJsonFull(jsonDecode(_msg));
+      file = oneFileData;
       e?.remove();
+      fileData = LineSplitter.split(await requestOnce(
+              '$wwwGetFileData${file.path}$msgRecordSeparator${file.encode}'))
+          .toList(growable: false);
     }
-    if (oneFileData.type != NOneFileDataType.las) {
+    if (file.type != NOneFileDataType.las) {
       return false;
     }
     if (e == null) {
       e = document.createElement('main')
-        ..classes.addAll(['opend-file', 'a-opening', 'las'])
+        ..classes.addAll(
+            ['mdc-top-app-bar--fixed-adjust', 'opend-file', 'a-opening', 'las'])
         ..append(DivElement()
-          ..classes.add('file-sets')
+          ..classes.add('file-sets-bar')
           ..append(SpanElement()
-            ..classes.add('file-sets-type')
-            ..innerText =
-                'Тип: ${file.type.toString().substring(file.type.runtimeType.toString().length)}')
+            ..classes.add('file-sets')
+            ..classes.add('size')
+            ..innerText = '${file.size} байт')
           ..append(SpanElement()
-            ..classes.add('file-sets-size')
-            ..innerText = 'Размер: ${file.size} байт'));
+            ..classes.add('file-sets')
+            ..classes.add('notes')
+            ..append(SpanElement()
+              ..classes.add('file-sets-notes')
+              ..classes.add('errors')
+              ..innerText = file.notesError?.toString() ?? '0')
+            ..append(SpanElement()
+              ..classes.add('file-sets-notes')
+              ..classes.add('warnings')
+              ..innerText = file.notesWarnings?.toString() ?? '0'))
+          ..append(
+              SpanElement()..classes.add('file-sets')..classes.add('space'))
+          ..append(SpanElement()
+            ..classes.add('file-sets')
+            ..classes.add('encode')
+            ..innerText = file.encode)
+          ..append(SpanElement()
+            ..classes.add('file-sets')
+            ..classes.add('type')
+            ..innerText = file.type
+                .toString()
+                .substring(file.type.runtimeType.toString().length)));
+      final _l = fileData.length;
+      for (var i = 0; i < _l; i++) {
+        final _lineData = fileData[i];
+        final _notes =
+            file.notes.where((e) => e.line == i + 1).toList(growable: false);
+        print('$i');
+        var _noteError = false;
+        var _noteWarning = false;
+        var _noteIgnore = false;
+        var _noteSection = false;
+        final _l = _notes.length;
+        for (var j = 0; j < _l; j++) {
+          _noteWarning |= _notes[j].text.startsWith('!W');
+          _noteError |= _notes[j].text.startsWith('!E');
+          _noteIgnore |= _notes[j].text.startsWith('!Pignore');
+          _noteSection |= _notes[j].text.startsWith('!Psection');
+          print(jsonEncode(_notes[j]));
+        }
+
+        final _eLine = DivElement()..classes.add('file-line');
+
+        final _eLineIndex = DivElement()
+          ..id = 'line-${i + 1}'
+          ..classes.add('line-index')
+          ..innerText = '${i + 1}';
+
+        final _eLineData = PreElement()
+          ..innerText = _lineData
+          ..classes.add('line-data');
+
+        if (_noteWarning) {
+          _eLineData.classes.add('warning');
+        }
+        if (_noteError) {
+          _eLineData.classes.add('error');
+        }
+        if (_noteSection) {
+          _eLineData.classes.add('section');
+        } else if (_noteIgnore) {
+          _eLineData.classes.add('ignore');
+        }
+
+        _eLine..append(_eLineIndex)..append(_eLineData);
+
+        if (_notes.isNotEmpty) {
+          final _eLineNotes = DivElement()..classes.add('line-notes');
+
+          for (var j = 0; j < _l; j++) {
+            final _note = _notes[j].text;
+            final _eNote = DivElement();
+            if (_note.startsWith('!W')) {
+              _eNote
+                ..classes.add('line-notes-w')
+                ..innerText = _note.substring(2);
+            }
+            if (_note.startsWith('!E')) {
+              _eNote
+                ..classes.add('line-notes-e')
+                ..innerText = _note.substring(2);
+            }
+            if (_note.startsWith('!P')) {
+              final _parses = _note.substring(2).split(msgRecordSeparator);
+              _eNote.innerText = _parses.first;
+              for (var k = 1; k < _parses.length; k++) {
+                _eNote.append(SpanElement()
+                  ..classes.add('line-notes-p')
+                  ..innerText = _parses[k]);
+              }
+            }
+            _eLineNotes.append(_eNote);
+          }
+
+          _eLine.append(_eLineNotes);
+        }
+        e.append(_eLine);
+      }
 
       e.addEventListener('animationend', (event) {
         if ((event as AnimationEvent).animationName == 'slideout') {
@@ -55,7 +157,8 @@ class FileLas {
       e.hidden = false;
     }
     closeAll('opend-file');
-    return false;
+    document.body.append(e);
+    return true;
   }
 
   FileLas._init();
