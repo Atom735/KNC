6ea65eb389dbd8e8b7f8a1f09c541c0f36683913
Gilyazeev Adel <Atom735.ag47@gmail.com>
2020-09-23 18:37:26 +0300

    from codebox commit

--- web/ts/index.tsx
+++ web/ts/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-navigator.serviceWorker.register('./sw.bundle.js', { scope: './' })
+navigator.serviceWorker
+  .register("./sw.bundle.js", { scope: "./" })
   .then((reg) => {
     // регистрация сработала
-    console.info('Registration succeeded. Scope is ' + reg.scope);
-  }).catch((error) => {
+    console.info("Registration succeeded. Scope is " + reg.scope);
+  })
+  .catch((error) => {
     // регистрация прошла неудачно
-    console.error('Registration failed with ' + error);
+    console.error("Registration failed with " + error);
   });
 
+window.onpopstate = function (event: PopStateEvent) {
+  console.log(document.location.toString());
+  console.dir(event.state);
+};
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root'),
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
