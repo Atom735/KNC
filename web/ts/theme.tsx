2a1c98e130883c71be0e15fc35dd0d09305469f1
Atom735 <Atom735.ag47@gmaIl.com>
2020-09-23 14:17:59 +0300

    update theme

--- web/ts/theme.tsx
+++ web/ts/theme.tsx
@@ -1,7 +1,17 @@
-import red from '@material-ui/core/colors/red';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { ruRU } from '@material-ui/core/locale';
+import { blue, pink } from '@material-ui/core/colors';
 
 // A custom theme for this app
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+    },
+    secondary: {
+      main: pink.A700,
+    },
+  },
+}, ruRU);
 
 export default theme;
