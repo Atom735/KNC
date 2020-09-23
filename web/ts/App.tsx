e9eb140c8825d9ddb7ed78e6c1870cdd4b8eb14e
Gilyazeev Adel <Atom735.ag47@gmail.com>
2020-09-23 17:10:24 +0300

    add Home page and Login on AppBar

--- web/ts/App.tsx
+++ web/ts/App.tsx
@@ -1,52 +1,62 @@
-import React, { FunctionComponent, useState } from 'react';
-import theme from './theme';
-import { makeStyles, Theme, createStyles, ThemeProvider } from '@material-ui/core/styles';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { FunctionComponent, useState } from "react";
+import theme from "./theme";
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  ThemeProvider
+} from "@material-ui/core/styles";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
-import Zoom from '@material-ui/core/Zoom';
-import Link from '@material-ui/core/Link';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Box from "@material-ui/core/Box";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+import Link from "@material-ui/core/Link";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
-import SignIn from './Signin';
-import SignUp from './Signup';
-import Test from './Test';
+import Home from "./Home";
+import SignIn from "./Signin";
+import SignUp from "./Signup";
+import Test from "./Test";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStylesScrollTop = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      position: 'fixed',
+      position: "fixed",
       bottom: theme.spacing(2),
-      right: theme.spacing(2),
-    },
-  }),
+      right: theme.spacing(2)
+    }
+  })
 );
 
-
 function ScrollTop() {
-  const classes = useStyles();
+  const classes = useStylesScrollTop();
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will default to window.
   // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 100,
+    threshold: 100
   });
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const anchor = ((event.target as HTMLDivElement).ownerDocument || document).querySelector(
-      '#back-to-top-anchor',
-    );
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector("#back-to-top-anchor");
 
     if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   };
 
@@ -61,20 +71,18 @@ function ScrollTop() {
   );
 }
 
-
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://github.com/Atom735/KNC/projects/3">
         Atom735
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
-
 
 // our components props accept a number for the initial value
 const Counter: FunctionComponent<{ initial?: number }> = ({ initial = 0 }) => {
@@ -82,30 +90,125 @@ const Counter: FunctionComponent<{ initial?: number }> = ({ initial = 0 }) => {
   // setClicks is a function that accepts either a number or a function returning
   // a number
   const [clicks, setClicks] = useState(initial);
-  return <>
-    <p>Clicks: {clicks}</p>
-    <button onClick={() => setClicks(clicks + 1)}>+</button>
-    <button onClick={() => setClicks(clicks - 1)}>-</button>
-  </>
-}
+  return (
+    <>
+      <p>Clicks: {clicks}</p>
+      <button onClick={() => setClicks(clicks + 1)}>+</button>
+      <button onClick={() => setClicks(clicks - 1)}>-</button>
+    </>
+  );
+};
+
+const useStylesApp = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    homeButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 const App: FunctionComponent = () => {
-  /// Указанная ссылка в приложении
+  const classes = useStylesApp();
+
   const [location, setLocation] = useState(window.location);
+  const [username, setUsername] = useState("");
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
   let page;
-  if (location.pathname.startsWith('/signin')) {
+  let pageHome;
+  let userAction;
+  if (location.pathname.startsWith("/signin")) {
     page = <SignIn />;
-  } else if (location.pathname.startsWith('/signup')) {
+  } else if (location.pathname.startsWith("/signup")) {
     page = <SignUp />;
-  } else if (location.pathname.startsWith('/test')) {
+  } else if (location.pathname.startsWith("/test")) {
     page = <Test />;
+  } else {
+    page = <Home />;
+    pageHome = true;
+  }
+
+  const handleUserMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleUserMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const handleSignIn = () => {};
+  const handleSignOut = () => {
+    handleUserMenuClose();
+    setUsername("");
+  };
+  const handleSettings = () => {
+    handleUserMenuClose();
+  };
+
+  if (username) {
+    userAction = (
+      <div>
+        <Typography variant="h6">{username}</Typography>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleUserMenuOpen}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right"
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right"
+          }}
+          open={open}
+          onClose={handleUserMenuClose}
+        >
+          <MenuItem onClick={handleSettings}>Настройки</MenuItem>
+          <MenuItem onClick={handleSignOut}>Выход</MenuItem>
+        </Menu>
+      </div>
+    );
+  } else {
+    userAction = (
+      <Button color="inherit" onClick={handleSignIn}>
+        Вход
+      </Button>
+    );
   }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar>
         <Toolbar>
-          <Typography variant="h6">Пункт приёма стеклотары</Typography>
+          {pageHome && (
+            <IconButton
+              edge="start"
+              className={classes.homeButton}
+              color="inherit"
+              aria-label="home"
+            >
+              <HomeIcon />
+            </IconButton>
+          )}
+          <Typography variant="h6" className={classes.title}>
+            Пункт приёма стеклотары
+          </Typography>
+          {userAction}
         </Toolbar>
       </AppBar>
       <Toolbar id="back-to-top-anchor" />
@@ -116,5 +219,5 @@ const App: FunctionComponent = () => {
       <ScrollTop />
     </ThemeProvider>
   );
-}
+};
 export default App;
--- web/ts/Signin.tsx
+++ web/ts/Signin.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
 
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
@@ -9,7 +8,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
