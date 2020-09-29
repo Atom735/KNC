2b9dd01382e0705557e752cd5cc01059ced35333
Atom735 <Atom735.ag47@gmail.com>
2020-09-29 20:32:38 +0300

    migrate string[] component to templComp

--- web/ts/pages/NewTask.tsx
+++ web/ts/pages/NewTask.tsx
@@ -10,6 +10,8 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
+import { GridProps } from "@material-ui/core/Grid";
+
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -45,7 +47,7 @@ const useStyles = makeStyles((theme) =>
   createStyles({
     input: {
       display: 'none',
-    },
+    }
   }),
 );
 
@@ -246,8 +248,8 @@ const NewTaskPaths: React.FC<NewTaskSetsChldProps> = (props) => {
 interface NumberTextFieldProps {
   value?: number;
   setValue: (value: number) => any;
-  default: number;
-  textFieldProps: TextFieldProps;
+  default?: number;
+  textFieldProps?: TextFieldProps;
 };
 
 const NumberTextField: React.FC<NumberTextFieldProps> = (props) => {
@@ -272,8 +274,13 @@ const NumberTextField: React.FC<NumberTextFieldProps> = (props) => {
   };
 
   const handleSetDefault = () => {
-    setState(props.default.toString());
-    props.setValue(props.default);
+    if (props.default) {
+      setState(props.default.toString());
+      props.setValue(props.default);
+    } else {
+      setState('');
+      props.setValue(0);
+    }
   }
 
   return (<TextField {...props.textFieldProps}
@@ -281,11 +288,10 @@ const NumberTextField: React.FC<NumberTextFieldProps> = (props) => {
     onChange={handleChange}
     InputProps={{
       endAdornment: <InputAdornment position="end">
-        <Tooltip title={<>{props.children}<br />
+        <Tooltip title={<>{props.children}<br /><br />
 Если в конце написать букву 'K' - то значение будет умножено на 2^10, 'М' - на 2^20, 'Г' - на 2^30.
-          <br />
-          <br />
-Чтобы настройки остались по умолчанию, оставте поле пустым или
+        <br /><br />
+            Чтобы настройки остались по умолчанию, оставте поле пустым или
 нажмите чтобы заменить настройками по умолчанию.</>}>
           <IconButton
             onClick={handleSetDefault}
@@ -298,6 +304,104 @@ const NumberTextField: React.FC<NumberTextFieldProps> = (props) => {
     }}
   />);
 }
+
+
+interface ArrayOfTextFieldsProps {
+  value?: string[];
+  setValue: (value: string[]) => any;
+  default?: string[];
+  itemProps?: GridProps;
+  textFieldProps?: TextFieldProps;
+  lastItemChilds?: React.ReactNode;
+};
+const ArrayOfTextFields: React.FC<ArrayOfTextFieldsProps> = (props) => {
+
+  const [state, setState] = useState<string[]>(props.value ? props.value.map((value) => value) : []);
+  const [indexesNew, setIndexesNew] = useState<number>(state.length + 1);
+  const [indexes, setIndexes] = useState<number[]>(state.map((value, index) => index + 1));
+
+  const handleChange = (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState(state.map(
+      (value, index) => id == index ? event.target.value : value));
+    props.setValue(state.map(
+      (value, index) => id == index ? event.target.value : value));
+  };
+
+  const handlePreventEvent = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleRemove = (id: number) => (event: React.MouseEvent<HTMLButtonElement>) => {
+    setState(state.filter((value, index) => index != id));
+    setIndexes(indexes.filter((value, index) => index != id));
+    props.setValue(state.filter((value, index) => index != id));
+  };
+
+  const handleAddNew = () => {
+    setState([...state, '']);
+    setIndexes([...indexes, indexesNew]);
+    setIndexesNew(indexesNew + 1);
+    props.setValue([...state, '']);
+  };
+
+  const handleSetDefault = () => {
+    if (props.default) {
+      setState([...props.default]);
+      setIndexes(props.default.map((value, index) => index + 1));
+      setIndexesNew(indexesNew + props.default.length);
+      props.setValue([...props.default]);
+    } else {
+      setState([]);
+      setIndexes([]);
+      props.setValue([]);
+    }
+  }
+
+  return (<>{
+    state.map((value, index) =>
+      <Grid item {...props.itemProps} key={indexes[index]} >
+        <TextField {...props.textFieldProps}
+          value={value}
+          onChange={handleChange(index)}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">
+              <Tooltip title="Удалить поле">
+                <IconButton
+                  onClick={handleRemove(index)}
+                  onMouseDown={handlePreventEvent}
+                >
+                  <RemoveCircleOutlineIcon />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>,
+          }}
+        />
+      </Grid>
+    )
+  }
+    <Grid item xs={12}>
+      <Tooltip title="Добавить новое поле">
+        <IconButton
+          onClick={handleAddNew}
+        >
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title={<>{props.children}
+        <br /><br />
+          Чтобы настройки остались по умолчанию, оставте поля пустымы или удалите все поля или
+нажмите чтобы заменить настройками по умолчанию.</>}>
+        <IconButton
+          onClick={handleSetDefault}
+          onMouseDown={handlePreventEvent}
+        >
+          <HelpOutlineIcon />
+        </IconButton>
+      </Tooltip>
+      {props.lastItemChilds}
+    </Grid>
+  </>);
+};
 
 
 
@@ -334,6 +438,18 @@ const PageNewTask: React.FC<RouterProps & PropsFromState & typeof mapDispatchToP
   }
   const handleChangeArDepth = (value: number) => {
     setSets({ ...sets, settings: { ...sets.settings, "ar-d": value } });
+  }
+  const handleChangeUpdateDuration = (value: number) => {
+    setSets({ ...sets, settings: { ...sets.settings, ud: value } });
+  }
+  const handleChangePaths = (value: string[]) => {
+    setSets({ ...sets, settings: { ...sets.settings, path: [...value] } });
+  }
+  const handleChangeArExt = (value: string[]) => {
+    setSets({ ...sets, settings: { ...sets.settings, "ar-e": [...value] } });
+  }
+  const handleChangeFileExt = (value: string[]) => {
+    setSets({ ...sets, settings: { ...sets.settings, "f-e": [...value] } });
   }
 
   return (
@@ -390,29 +506,33 @@ const PageNewTask: React.FC<RouterProps & PropsFromState & typeof mapDispatchToP
                       label={sets.public ? "Публичная задача" : "Приватная задача"} />
                   </Grid>
 
-                  <NewTaskPaths sets={sets} setSets={setSets} />
 
-                  <Grid item>
-                    <input
-                      accept="*"
-                      className={classes.input}
-                      id="contained-button-file"
-                      multiple
-                      type="file"
-                    />
-                    <label htmlFor="contained-button-file">
-
-                      <Tooltip title="Прикрепить файл">
-                        <IconButton component="span">
-                          <AttachFileIcon />
-                        </IconButton>
-                      </Tooltip>
-
-                    </label>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">Сканируемые пути к папкам и файлам</Typography>
                   </Grid>
+                  <ArrayOfTextFields setValue={handleChangePaths} default={JTaskSettings_defs.path} itemProps={{ xs: 12 }}
+                    textFieldProps={{ fullWidth: true, label: "Сканируемый путь" }} lastItemChilds={<>
+                      <input
+                        accept="*"
+                        className={classes.input}
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                      />
+                      <label htmlFor="contained-button-file">
 
+                        <Tooltip title="Прикрепить файл">
+                          <IconButton component="span">
+                            <AttachFileIcon />
+                          </IconButton>
+                        </Tooltip>
 
-
+                      </label></>}>
+                    Указывает пути к файлам и папкам, которые просканируем.<br />
+                      Сейчас введено: {sets.settings.path && !sets.settings.path.every((value) => !value) ?
+                      sets.settings.path.map((value, index) => <span key={index}><br />[{index}] {value}</span>)
+                      : JTaskSettings_defs.path.map((value, index) => <span key={index}><br />[{index}] {value}</span>)}
+                  </ArrayOfTextFields>
                 </Grid>
               </AccordionDetails>
             </Accordion>
@@ -423,14 +543,24 @@ const PageNewTask: React.FC<RouterProps & PropsFromState & typeof mapDispatchToP
               <AccordionDetails>
                 <Grid container spacing={2}>
                   <Grid item xs={12}>
-                    <NumberTextField setValue={handleChangeArSize} default={JTaskSettings_defs.ud}
+                    <Typography variant="h6">Расширения обрабатываемых файлов</Typography>
+                  </Grid>
+                  <ArrayOfTextFields setValue={handleChangeFileExt} default={JTaskSettings_defs["f-e"]} itemProps={{ xs: 6, sm: 3, lg: 2 }}
+                    textFieldProps={{ fullWidth: true }}>
+                    Указывает файлы с каким расширением будут обработаны программой.<br />
+                      Сейчас введено: {sets.settings["f-e"] && !sets.settings["f-e"].every((value) => !value) ?
+                      sets.settings["f-e"].map((value, index) => <span key={index}><br />[{index}] {value}</span>)
+                      : JTaskSettings_defs["f-e"].map((value, index) => <span key={index}><br />[{index}] {value}</span>)}
+                  </ArrayOfTextFields>
+                  <Grid item xs={12} sm={4}>
+                    <NumberTextField setValue={handleChangeUpdateDuration} default={JTaskSettings_defs.ud}
                       textFieldProps={{ fullWidth: true, label: "Интервал обновления" }}>
                       Указывает время в миллисекундах между отправками обновлённых данных задачи.<br />
                       Сейчас введено {sets.settings.ud ? sets.settings.ud.toString() : JTaskSettings_defs.ud} мс.
                       </NumberTextField>
                   </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <NumberTextField setValue={handleChangeArSize} default={JTaskSettings_defs["ar-d"]}
+                  <Grid item xs={12} sm={4}>
+                    <NumberTextField setValue={handleChangeArDepth} default={JTaskSettings_defs["ar-d"]}
                       textFieldProps={{ fullWidth: true, label: "Вложенность архивов" }}>
                       Указывает глубину раскрытия архивов. Если `1` то архивы внутри архивов не будут вскрываться.<br />
                       `-1` - для бесконечной вложенности<br />
@@ -439,7 +569,7 @@ const PageNewTask: React.FC<RouterProps & PropsFromState & typeof mapDispatchToP
                       Сейчас введено {sets.settings["ar-d"] ? sets.settings["ar-d"].toString() : JTaskSettings_defs["ar-d"]}.
                       </NumberTextField>
                   </Grid>
-                  <Grid item xs={12} sm={6}>
+                  <Grid item xs={12} sm={4}>
                     <NumberTextField setValue={handleChangeArSize} default={JTaskSettings_defs["ar-s"]}
                       textFieldProps={{ fullWidth: true, label: "Размер архива" }}>
                       Указывает файлы какого максимального размера будут вскрыты архиватором.<br />
@@ -450,7 +580,13 @@ const PageNewTask: React.FC<RouterProps & PropsFromState & typeof mapDispatchToP
                   <Grid item xs={12}>
                     <Typography variant="h6">Расширения архивных файлов</Typography>
                   </Grid>
-                  <NewTaskArchiveExt sets={sets} setSets={setSets} />
+                  <ArrayOfTextFields setValue={handleChangeArExt} default={JTaskSettings_defs["ar-e"]} itemProps={{ xs: 6, sm: 3, lg: 2 }}
+                    textFieldProps={{ fullWidth: true }}>
+                    Указывает файлы с каким расширением будут вскрываться архиватором.<br />
+                      Сейчас введено: {sets.settings["ar-e"] && !sets.settings["ar-e"].every((value) => !value) ?
+                      sets.settings["ar-e"].map((value, index) => <span key={index}><br />[{index}] {value}</span>)
+                      : JTaskSettings_defs["ar-e"].map((value, index) => <span key={index}><br />[{index}] {value}</span>)}
+                  </ArrayOfTextFields>
                 </Grid>
               </AccordionDetails>
             </Accordion>
