2230e1d99402d626b25088885ff5bd670747578c
Atom735 <Atom735.ag47@gmail.com>
2020-10-06 16:00:22 +0300

    try for use virtualized table

--- web/ts/pages/TaskFileList.tsx
+++ web/ts/pages/TaskFileList.tsx
@@ -28,6 +28,164 @@ import { useSnackbar } from "notistack";
 import { rTaskStateLinearProgress, rTaskStateString } from "../cards/Task";
 import { useStylesApp } from "../App";
 import Tooltip from "@material-ui/core/Tooltip";
+import clsx from 'clsx';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import { AutoSizer, Column, Table, TableCellRenderer, TableHeaderProps } from 'react-virtualized';
+import { JOneFileData } from "../dart/OneFileData";
+
+
+declare module '@material-ui/core/styles/withStyles' {
+  // Augment the BaseCSSProperties so that we can control jss-rtl
+  interface BaseCSSProperties {
+    /*
+     * Used to control if the rule-set should be affected by rtl transformation
+     */
+    flip?: boolean;
+  }
+}
+
+const styles = (theme: Theme) =>
+  createStyles({
+    flexContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      boxSizing: 'border-box',
+    },
+    table: {
+      // temporary right-to-left patch, waiting for
+      // https://github.com/bvaughn/react-virtualized/issues/454
+      '& .ReactVirtualized__Table__headerRow': {
+        flip: false,
+        paddingRight: theme.direction === 'rtl' ? '0 !important' : undefined,
+      },
+    },
+    tableRow: {
+      cursor: 'pointer',
+    },
+    tableRowHover: {
+      '&:hover': {
+        backgroundColor: theme.palette.grey[200],
+      },
+    },
+    tableCell: {
+      flex: 1,
+    },
+    noClick: {
+      cursor: 'initial',
+    },
+  });
+
+interface ColumnData {
+  dataKey: string;
+  label: string;
+  numeric?: boolean;
+  width: number;
+}
+
+interface Row {
+  index: number;
+}
+
+interface MuiVirtualizedTableProps extends WithStyles<typeof styles> {
+  columns: ColumnData[];
+  headerHeight?: number;
+  onRowClick?: () => void;
+  rowCount: number;
+  rowGetter: (row: Row) => JOneFileData;
+  rowHeight?: number;
+}
+
+class MuiVirtualizedTable extends React.PureComponent<MuiVirtualizedTableProps> {
+  static defaultProps = {
+    headerHeight: 48,
+    rowHeight: 48,
+  };
+
+  getRowClassName = ({ index }: Row) => {
+    const { classes, onRowClick } = this.props;
+
+    return clsx(classes.tableRow, classes.flexContainer, {
+      [classes.tableRowHover]: index !== -1 && onRowClick != null,
+    });
+  };
+
+  cellRenderer: TableCellRenderer = ({ cellData, columnIndex }) => {
+    const { columns, classes, rowHeight, onRowClick } = this.props;
+    return (
+      <TableCell
+        component="div"
+        className={clsx(classes.tableCell, classes.flexContainer, {
+          [classes.noClick]: onRowClick == null,
+        })}
+        variant="body"
+        style={{ height: rowHeight }}
+        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
+      >
+        {cellData}
+      </TableCell>
+    );
+  };
+
+  headerRenderer = ({ label, columnIndex }: TableHeaderProps & { columnIndex: number }) => {
+    const { headerHeight, columns, classes } = this.props;
+
+    return (
+      <TableCell
+        component="div"
+        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+        variant="head"
+        style={{ height: headerHeight }}
+        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+      >
+        <span>{label}</span>
+      </TableCell>
+    );
+  };
+
+  render() {
+    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <Table
+            height={height}
+            width={width}
+            rowHeight={rowHeight!}
+            gridStyle={{
+              direction: 'inherit',
+            }}
+            headerHeight={headerHeight!}
+            className={classes.table}
+            {...tableProps}
+            rowClassName={this.getRowClassName}
+          >
+            {columns.map(({ dataKey, ...other }, index) => {
+              return (
+                <Column
+                  key={dataKey}
+                  headerRenderer={(headerProps) =>
+                    this.headerRenderer({
+                      ...headerProps,
+                      columnIndex: index,
+                    })
+                  }
+                  className={classes.flexContainer}
+                  cellRenderer={this.cellRenderer}
+                  dataKey={dataKey}
+                  {...other}
+                />
+              );
+            })}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
+
+const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
 
 
@@ -49,6 +207,9 @@ const PageTaskFileList: React.FC<PageTaskFileListProps & typeof mapDispatchToPro
 
   const [task, setTask] = useState(props.tasks.find((value) => value.id == _taskId));
 
+  const [files, setFiles] = useState(task?.filelist);
+
+
   useEffect(() => {
     props.fetchSetTitle('Список файлов задачи: ' + task?.id);
   }, [task]);
@@ -61,6 +222,7 @@ const PageTaskFileList: React.FC<PageTaskFileListProps & typeof mapDispatchToPro
           enqueueSnackbar("Невозможно получить список файлов: " + msg, { variant: "error" });
         } else {
           fetchTaskUpdateFileList(msg, task?.id);
+          setFiles(JSON.parse(msg));
         }
       });
     }
@@ -70,12 +232,66 @@ const PageTaskFileList: React.FC<PageTaskFileListProps & typeof mapDispatchToPro
     setTask(props.tasks.find((value) => value.id == _taskId));
   }, [props.tasks]);
 
+  useEffect(() => {
+    // setFiles(task?.filelist);
+  }, [task]);
+
 
 
   return (
     <Container component="main">
       <CssBaseline />
-    </Container>
+      {!files ? null :
+        < Paper style={{ height: '100%', width: '100%' }}>
+          <VirtualizedTable
+            rowCount={files.length}
+            rowGetter={({ index }) => files[index]}
+            columns={[
+              {
+                width: 200,
+                label: 'Оригинал',
+                dataKey: 'origin',
+              },
+              {
+                width: 120,
+                label: 'Копия',
+                dataKey: 'path',
+              },
+              {
+                width: 120,
+                label: 'Тип',
+                dataKey: 'type',
+              },
+              {
+                width: 120,
+                label: 'Размер',
+                dataKey: 'size',
+                numeric: true,
+              },
+              {
+                width: 120,
+                label: 'Кодировка',
+                dataKey: 'encode',
+                numeric: true,
+              },
+              {
+                width: 120,
+                label: 'Предупреждений',
+                dataKey: 'n-warn',
+                numeric: true,
+              },
+              {
+                width: 120,
+                label: 'Ошибок',
+                dataKey: 'n-errors',
+                numeric: true,
+              },
+            ]}
+          />
+
+        </Paper>
+      }
+    </Container >
   );
 };
 
