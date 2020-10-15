4ed0d3e8b1ae7d4be45adf79c69a44892db4df91
Atom735 <Atom735.ag47@gmail.com>
2020-10-15 18:27:46 +0300

    add dbf loader

--- lib/src/dbf.dart
+++ lib/src/dbf.dart
@@ -1,21 +1,38 @@
 import 'dart:typed_data';
+import 'dbf.g.dart';
 
-
-extension ExtDbfFieldDesc on DbfFieldDesc {
-  /*late*/ String name;
-  /*late*/ String type;
-  /*late*/ int address;
-  /*late*/ int length;
-  /*late*/ int decimalCount;
-
-  /// Загружает данные из буффера байтов
-  ///
-  /// Возвращает `true`  если получилось разобрать байты корректно
-  bool loadByByteData(final ByteData bytes) {
+extension IDbfHead on DbfHead {
+  static DbfHead /*?*/ createByByteData(final ByteData bytes) {
     if (bytes.lengthInBytes < 32) {
-      return false;
+      return null;
     }
+    return DbfHead(
+        bytes.getUint8(0),
+        bytes.getUint8(1),
+        bytes.getUint8(2),
+        bytes.getUint8(3),
+        bytes.getUint32(4, Endian.little),
+        bytes.getUint16(8, Endian.little),
+        bytes.getUint16(10, Endian.little),
+        bytes.getUint16(12, Endian.little),
+        bytes.getUint8(14),
+        bytes.getUint8(15),
+        bytes.getUint32(16, Endian.little),
+        bytes.getUint32(20, Endian.little),
+        bytes.getUint32(24, Endian.little),
+        bytes.getUint8(28),
+        bytes.getUint8(29),
+        bytes.getUint16(30),
+        '',
+        0);
+  }
+}
 
+extension IDbfFieldStruct on DbfFieldStruct {
+  static DbfFieldStruct /*?*/ createByByteData(final ByteData bytes) {
+    if (bytes.lengthInBytes < 32) {
+      return null;
+    }
     var ij = -1;
     for (var i = 0; i <= 10 && ij == -1; i++) {
       if (bytes.getUint8(i) == 0) {
@@ -25,71 +42,58 @@ extension ExtDbfFieldDesc on DbfFieldDesc {
     if (ij == -1) {
       ij = 11;
     }
-    name =
+    final _name =
         String.fromCharCodes(bytes.buffer.asInt8List(bytes.offsetInBytes, ij));
-    type = String.fromCharCode(bytes.getUint8(11));
-    address = bytes.getUint32(12, Endian.little);
-    length = bytes.getUint8(16);
-    decimalCount = bytes.getUint8(17);
-    return true;
+    final _type = String.fromCharCode(bytes.getUint8(11));
+    return DbfFieldStruct(
+        _name,
+        _type,
+        bytes.getUint32(12, Endian.little),
+        bytes.getUint8(16),
+        bytes.getUint8(17),
+        bytes.getUint8(18),
+        bytes.getUint32(19, Endian.little),
+        bytes.getUint8(23),
+        bytes.getUint32(24, Endian.little),
+        bytes.getUint32(28, Endian.little));
   }
 }
 
-extension ExtDbfFieldDesc on DbfFieldDesc {
-  /// Загружает данные из буффера байтов
-  ///
-  /// Возвращает `true`  если получилось разобрать байты корректно
-  bool loadByByteData(final ByteData bytes) {
-    if (bytes.lengthInBytes < 30) {
-      return false;
+extension IDbfRecord on DbfRecord {
+  static DbfRecord /*?*/ createByByteData(
+      final ByteData bytes, final List<DbfFieldStruct> fields) {}
+}
+
+extension IOneFileDbf on OneFileDbf {
+  /// Загружает данные из буфера байтов
+  static OneFileDbf /*?*/ createByByteData(final ByteData bytes) {
+    /// Если в базе данных отсутсвует заголовок и хотяб одно поле
+    if (bytes.lengthInBytes < 65) {
+      return null;
     }
-    signature = bytes.getUint8(0);
-    if (signature != 3) {
-      return false;
+    final _head = IDbfHead.createByByteData(bytes);
+
+    /// Если размеры не соответсуют укзаанным
+    if (bytes.lengthInBytes <
+        _head.lengthOfHeader +
+            _head.lengthOfEachRecord * _head.numberOfRecords) {
+      return null;
     }
-    lastUpdateYY = bytes.getUint8(1);
-    lastUpdateMM = bytes.getUint8(2);
-    lastUpdateDD = bytes.getUint8(3);
-    numberOfRecords = bytes.getUint32(4, Endian.little);
-    lengthOfHeader = bytes.getUint16(8, Endian.little);
-    lengthOfEachRecord = bytes.getUint16(10, Endian.little);
-    incompleteTransac = bytes.getUint8(14);
-    ecryptionFlag = bytes.getUint8(15);
-    freeRecordThread = bytes.getUint32(16, Endian.little);
-    mdxFlag = bytes.getUint8(28);
-    laguageDriver = bytes.getUint8(29);
+    final _fields = <DbfFieldStruct>[];
+
     var offset = 32;
     while (bytes.getUint8(offset) != 0x0D) {
-      final field = DbfFieldDesc();
-      if (!field
-          .loadByByteData(ByteData.sublistView(bytes, offset, offset + 32))) {
-        return false;
-      }
-      fields.add(field);
+      _fields.add(IDbfFieldStruct.createByByteData(
+          ByteData.sublistView(bytes, offset, offset + 32)));
       offset += 32;
     }
-
-    if (bytes.lengthInBytes <
-        lengthOfHeader + lengthOfEachRecord * numberOfRecords) {
-      return false;
-    }
-
-    offset = lengthOfHeader;
-    final _filedsLength = fields.length;
-    // final _recordOffset = fields.fold<int>(1, (v, e) => v += e.length);
-    records = List.generate(numberOfRecords, (i) {
-      final _list = List.filled(_filedsLength + 1, '');
-      offset = lengthOfHeader + lengthOfEachRecord * i;
-      _list[0] = String.fromCharCode(bytes.getUint8(offset));
-      offset += 1;
-      for (var j = 0; j < _filedsLength; j++) {
-        _list[j + 1] = String.fromCharCodes(bytes.buffer
-            .asUint8List(bytes.offsetInBytes + offset, fields[j].length));
-        offset += fields[j].length;
-      }
-      return _list;
+    final _records = List<DbfRecord>.generate(_head.numberOfRecords, (i) {
+      offset = _head.lengthOfHeader + _head.lengthOfEachRecord * i;
+      return IDbfRecord.createByByteData(
+          ByteData.sublistView(
+              bytes, offset, offset + _head.lengthOfEachRecord),
+          _fields);
     });
-
-    return true;
+    return OneFileDbf(_head, _fields, _records);
   }
 }
