a653e4f45ce6ad4f678448babbc7c3c6291b12c1
Atom735 <Atom735.ag47@gmail.com>
2020-11-11 16:19:08 +0300

    add tests for dbf code

---
+++ bin/tests/dbf.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:knc/src/dbf/index.dart';
+
+void main(List<String> args) {
+  final pathList = File(r'.ignore\files\dbf\__out.txt').readAsLinesSync()
+    ..removeWhere((e) => e.isEmpty);
+  for (var path in pathList) {
+    final data = ByteData.sublistView(File(path).readAsBytesSync());
+    if (Dbf.validate(data)) {
+      final dbf = Dbf(data);
+      File(path + '.txt').writeAsStringSync('\uFEFF' + dbf.debugStringFull);
+    }
+  }
+}
