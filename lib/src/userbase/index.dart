9b7523cda9450db0e0cfad5e685f1b8d50e59eb5
Atom735 <Atom735.ag47@gmail.com>
2020-11-13 16:53:18 +0300

    add userbase

---
+++ lib/src/userbase/index.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
+
+import 'structs.dart';
+
+final _fileBase = File('data/users.json');
+
+/// key - email
+final Map<String, User> userbaseUsers = {};
+
+/// key - token
+final Map<String, UserSessionToken> userbaseTokens = {};
+
+/// Преоразует строку пароля в шифрованный пароль
+String passwordEncode(final String pass) => sha256.convert([
+      ...'0x834^'.codeUnits,
+      ...pass.codeUnits,
+      ...'x12kdasdj'.codeUnits
+    ]).toString();
+
+/// Загружает данные всех пользователей
+void userbaseLoad() {
+  userbaseUsers.clear();
+  userbaseTokens.clear();
+  if (_fileBase.existsSync()) {
+    final map = jsonDecode(_fileBase.readAsStringSync());
+    final mapUsers = map['users'] as List;
+    final mapTokens = map['tokens'] as List;
+
+    for (final mapUser in mapUsers) {
+      final user = User.fromJson(mapUser);
+      userbaseUsers[user.email.toLowerCase()] = user;
+    }
+
+    for (final mapToken in mapTokens) {
+      final email = mapToken['email'];
+      final token = UserSessionToken.fromJson(
+          mapToken, userbaseUsers[email.toLowerCase()]);
+      userbaseTokens[token.token] = token;
+    }
+  }
+}
+
+/// Загружает данные всех пользователей
+void userbaseSave() => _fileBase.writeAsStringSync(jsonEncode({
+      'users': [...userbaseUsers.values.map((e) => e.toJson())],
+      'tokens': [...userbaseTokens.values.map((e) => e.toJson())]
+    }));
+
+/// Создать нового пользователя
+User userNew(final User user) {
+  if (userbaseUsers.containsKey(user.email.toLowerCase())) {
+    throw Exception('Этот email уже зарегестрирован');
+  }
+  userbaseUsers[user.email.toLowerCase()] = user;
+}
+
+int _userTokenGeneratorCount = 0;
+
+/// Получает новый ключ сессии для пользователя
+UserSessionToken userNewToken(final User user) {
+  final token = sha512.convert([
+    ..._userTokenGeneratorCount.toString().codeUnits,
+    ...jsonEncode(user.toJson()).codeUnits,
+    ...DateTime.now().toString().codeUnits
+  ]).toString();
+  if (userbaseTokens.containsKey(token)) {
+    return userNewToken(user);
+  }
+  return userbaseTokens[token] = UserSessionToken(user, token, user.access);
+}
---
+++ lib/src/userbase/structs.dart
@@ -0,0 +1,97 @@
+/// Класс содержащий данные о пользователе
+/// name fname mname lname	name (full name) given-name (first name) additional-name (middle name) family-name (last name)
+class User {
+  /// name (full name)
+  ///
+  /// Полное имя
+  final String name;
+
+  /// given-name (first name)
+  ///
+  /// Имя
+  final String fname;
+
+  /// additional-name (middle name)
+  ///
+  /// Отчество
+  final String mname;
+
+  /// family-name (last name)
+  ///
+  /// Фамилия
+  final String lname;
+
+  /// Почта пользователя
+  final String email;
+
+  /// Номер телефона
+  final String phone;
+
+  /// Пароль зашифрованный через функцию [passwordEncode]
+  final String pass;
+
+  /// Символы доступа пользователя
+  final String access;
+
+  /// Ключи доступа
+  final tokens = <UserSessionToken>[];
+
+  User(
+    this.pass, {
+    this.name = '',
+    this.fname = '',
+    this.mname = '',
+    this.lname = '',
+    this.email = '',
+    this.phone = '',
+    this.access = '',
+  });
+
+  factory User.fromJson(Map<String, dynamic> map) => User(
+        map['pass'],
+        name: map['name'] ?? '',
+        fname: map['fname'] ?? '',
+        mname: map['mname'] ?? '',
+        lname: map['lname'] ?? '',
+        email: map['email'] ?? '',
+        phone: map['phone'] ?? '',
+        access: map['access'] ?? '',
+      );
+
+  Map<String, dynamic> toJson() => {
+        'pass': pass,
+        'name': name,
+        'fname': fname,
+        'mname': mname,
+        'lname': lname,
+        'email': email,
+        'phone': phone,
+        'access': access,
+      };
+}
+
+class UserSessionToken {
+  /// Почта пользователя
+  final User user;
+
+  /// Сам токен
+  final String token;
+
+  /// Символы доступа токена
+  final String access;
+
+  UserSessionToken(this.user, this.token, this.access);
+
+  factory UserSessionToken.fromJson(Map<String, dynamic> map, User user) =>
+      UserSessionToken(
+        user,
+        map['token'],
+        map['access'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'email': user.email,
+        'token': token,
+        'access': access,
+      };
+}
