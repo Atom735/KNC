22927a59d639f6eb950e835ad037ba7181eef20d
Atom735 <Atom735.ag47@gmail.com>
2020-11-18 19:15:44 +0300

    delete depricated

--- lib/src/ws/index.d.ts
+++ lib/src/ws/index.d.ts
@@ -1,31 +1,3 @@
-declare function JsSocketWrapper
-    (token: string,
-        callback: () => void):
-    Object;
-declare function JsSocketWrapperAddOnOpenCallback
-    (socketWrapper: Object,
-        callback: () => void):
-    void;
-declare function JsSocketWrapperSend
-    (socketWrapper: Object, id: number, msg: string):
-    void;
-declare function JsSocketWrapperRecv
-    (socketWrapper: Object, msg: string, id: number):
-    boolean;
-declare function JsSocketWrapperWaitMsg
-    (socketWrapper: Object, msg: string, sync: boolean,
-        callback: (id: number, msg: string) => void):
-    void;
-declare function JsSocketWrapperWaitMsgAll
-    (socketWrapper: Object, msg: string, sync: boolean,
-        callback: (id: number, msg: string) => void):
-    () => void;
-declare function JsSocketWrapperRequestOnce
-    (socketWrapper: Object, msg: string, sync: boolean,
-        callback: (msg: string) => void):
-    void;
-
-
 declare function JsSocketWrapper(token: string,
     callback: (t: JsSocketWrapper) => void): JsSocketWrapper;
 
